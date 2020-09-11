61df3c5789a73837f7293df9cd06daee281b1d08
Jonathan Bernstein <jjbernstein@gmail.com>
2020-09-11 15:33:10 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,7 @@
 # code your #position_taken? method here!
 
 def position_taken?(board, position)
-  if board[position] == " " || ""
+  if board[position] == " " || "" || nil
     return FALSE
 else
     return TRUE
